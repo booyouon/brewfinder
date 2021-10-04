b70e921e612cbf0059998f555bffc96c2b2a2835
Vince Abuyuan <vinceabuyuan@Vinces-MacBook-Air.local>
2021-10-04 10:56:43 -0700

    media queries

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const header = document.querySelector("body > header");
 const searchForm = document.querySelector("#search__form");
 const headerH1 = document.querySelector("body > header > h1");
 const domain = "https://api.openbrewerydb.org/breweries";
-const linkPrevDomain =
-  "http://api.linkpreview.net/?key=f14050be5f1332fc8bd9e8d9f84db942&q=";
+const microlinkDomain = "https://api.microlink.io?url=";
 const submitButton = document.querySelector("#searchSubmit");
 const searchHeading = document.querySelector(".main__h3");
 const searchContainer = document.querySelector(".searchContainer");
 const pageBtns = document.querySelector(".pageBtns");
+const beerStockPhoto =
+  "https://images.unsplash.com/photo-1535958636474-b021ee887b13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80";
 let pageCounter;
 
 // when the submit button is press the innerHtml of the searchcontainer will be cleared if there is any
@@ -20,7 +21,7 @@ let pageCounter;
 submitButton.addEventListener("click", (ev) => {
   header.classList.remove("headerNl");
   header.className = "headerSearch";
-  headerH1.style.marginRight = "25px";
+  headerH1.classList.add("header__h1Search");
   searchForm.style.margin = "0";
   pageCounter = 0;
   ev.preventDefault();
@@ -39,7 +40,6 @@ const fetchBrewData = (domain) =>
       return res.json();
     })
     .then((resjson) => {
-      console.log(resjson);
       domUpdate(resjson);
 
       // If api content that comes back is greater than 10, then the nextBtn and prevBtn allow to dynamically update the page
@@ -81,54 +81,62 @@ const fetchBrewData = (domain) =>
       console.log(`Error: ${e}`);
     });
 
-const fetchLinkPreview = (domain) => {
-  fetch(domain)
-    .then((res) => {
-      return res.json();
-    })
-    .then((resjson) => {
-      console.log(resjson);
-    })
-    .catch((e) => {
-      console.log(`Error: ${e}`);
-    });
-};
-
 // domElCreate is used to specify what will be created inside domUpdate
 const domElCreate = (data, num) => {
   const searchDiv = document.createElement("div");
+  const infoDiv = document.createElement("div");
   const p = document.createElement("p");
   const h3 = document.createElement("h3");
+  const brewImg = document.createElement("img");
+  infoDiv.className = "infoDiv";
   searchDiv.className = "searchDiv";
+  brewImg.className = "brewImg";
+  brewImg.src = beerStockPhoto;
   h3.innerText = `${num + 1}. ${data[num].name}`;
   p.innerHTML = `${data[num].brewery_type} in ${data[num].city}, ${data[num].state}`;
   p.style.textTransform = "capitalize";
   searchContainer.append(searchDiv);
-  searchDiv.append(h3, p);
+  searchDiv.append(brewImg);
+  searchDiv.append(infoDiv);
+  infoDiv.append(h3, p);
   if (data[num].street) {
     const address = document.createElement("address");
-    address.innerHTML = `${data[num].street}, ${data[num].state}, ${data[num].state}`;
-    searchDiv.append(address);
+    address.innerHTML = `${data[num].street}, ${data[num].state}, ${data[num].state} ${data[num].postal_code}`;
+    infoDiv.append(address);
   }
   if (data[num].phone) {
     const phone = document.createElement("a");
     phone.innerText = formatPhone(data[num].phone);
     phone.href = `tel:${data[num].phone}`;
-    searchDiv.append(phone);
+    infoDiv.append(phone);
   }
   if (data[num].website_url) {
-    const website = document.createElement("a");
-    website.innerText = data[num].website_url;
-    website.href = data[num].website_url;
-    searchDiv.append(website);
-    h3.innerText = "";
-    const a = document.createElement("a");
-    a.innerText = `${num + 1}. ${data[num].name}`;
-    a.href = "#";
-    h3.append(a);
-    a.addEventListener("click", () => {
-      fetchLinkPreview(linkPrevDomain + data[num].website_url);
-    });
+    // fetches website data using the microlink API
+    fetch(microlinkDomain + data[num].website_url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resjson) => {
+        // checks if the statuscode of the website is 200 and if so then the website is added
+        if (resjson.statusCode === 200) {
+          console.log(resjson.data.image.url);
+          brewImg.src = resjson.data.image.url;
+          const website = document.createElement("a");
+          website.innerText = h3.innerText;
+          website.href = data[num].website_url;
+          h3.innerText = "";
+          h3.append(website);
+        }
+      })
+      .catch((e) => {
+        console.log(`Error: ${e}`);
+      });
+    // const a = document.createElement("a");
+    // a.innerText = `${num + 1}. ${data[num].name}`;
+    // a.href = "#";
+    // searchDiv.append(a);
+    // a.addEventListener("click", () => {
+    // });
   }
 };
 // function to format each phone number input
